a61779c05b58298f210bbdcd25bb482758fba1b6
Anatolii <anatolii.melchenko@academysmart.com>
2023-03-28 17:19:18 +0300

    imports;

--- src/main/java/com/example/kafka/rest/MessageController.java
+++ src/main/java/com/example/kafka/rest/MessageController.java
@@ -1,10 +1,12 @@
 package com.example.kafka.rest;
 
-import com.example.kafka.dto.UserDto;
 import com.example.kafka.dto.message.MessageDto;
 import com.example.kafka.service.micro.http.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("messages")
